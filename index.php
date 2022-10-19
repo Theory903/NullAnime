4dd67a4d1c9544c95ad42c1966bcb5b0bf05d0c1
Abhishek Jha <abhijha903@gmail.com>
2022-10-19 12:47:41 +0530

    edit

--- index.php
+++ index.php
@@ -76,6 +76,7 @@ require('./_config.php');
         </div>
         <!--End: Footer-->
     </div>
+    <img src="files/images/banne.png"
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     <script>
